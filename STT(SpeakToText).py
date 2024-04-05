6b54970baa83413785d6fd7612ed20bc73d34199
Yutak_Seo <soano2428@gmail.com>
2024-04-05 17:02:23 +0900

    24.04.05 refac complete

--- STT(SpeakToText).py
+++ STT(SpeakToText).py
@@ -1,29 +1,25 @@
 import speech_recognition as sr
-import pyaudio
 
 class STT:
     def __init__(self):
-        pass
-
-    rec_static_instance = None
-    @staticmethod
-    def rec_instance(self):
         self.rec_static_instance = sr.Recognizer()
-    
+        
     def get_audio(self):
         rec = self.rec_static_instance
         with sr.Microphone() as source:
-            audio_data = sr.listen(source)
+            audio_data = rec.listen(source)
             try:
-                text = rec.recognize_google(audio_data, language = "ko")
+                text = rec.recognize_google(audio_data, language="ko")
             except Exception as e:
-                pass
-        
+                text = "...listening"
         return text
     
     def run(self):
         while True:
-            text =  self.get_audio()
-            print(text)
-            
-STT.run()
+            text = self.get_audio()
+            if text:
+                print(text)
+
+
+temp = STT()
+temp.run()
