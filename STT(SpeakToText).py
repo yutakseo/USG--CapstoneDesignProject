79f9aa2da78203f35d7e1e52bde40bdd5031cba1
Yutak_Seo <soano2428@gmail.com>
2024-04-05 19:49:01 +0900

    24.04.05 new

--- STT(SpeakToText).py
+++ STT(SpeakToText).py
@@ -1,4 +1,4 @@
-import speech_recognition as sr
+import speech_recognition as sr, time
 
 class STT:
     def __init__(self):
@@ -9,8 +9,8 @@ class STT:
         rec = self.rec_static_instance
         with sr.Microphone() as source:
             try:
-                audio_data = rec.listen(source, timeout=1.3)
-                text = rec.recognize_google(audio_data, language="ko")
+                audio_data = rec.listen(source, timeout=0.4)
+                text = rec.recognize_google(audio_data, language="en-US") #ko
             except Exception as e:
                 text = "..."
         return text
@@ -22,7 +22,7 @@ class STT:
                 print("\n음성 인식을 종료합니다")
                 break
             else:
-                print(text)
+                print(f"\n{time.strftime('%I:%M:%S')} - ", text)
 
 temp = STT()
 temp.run()
