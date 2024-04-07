1d141f1e3e8664cef005d1150ba7e2a04de1f5d0
ucoco <78952628+ucoco@users.noreply.github.com>
2024-04-07 16:56:15 +0900

    24.04.07

--- OpenAIGPT.py
+++ OpenAIGPT.py
@@ -1,20 +1,36 @@
 from openai import OpenAI #pip install openai
 
-api_key = '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'
+class GPT:
+    def __init__(self, key):
+        self.key = key
+        self.prompt = None
+        self.client = OpenAI(api_key=self.key)
+        self.answer = None
 
-if api_key is None:
-    print("API 키를 설정해야 합니다. OPENAI_API_KEY 환경 변수를 설정해주세요.")
-    exit()
+    def gptInput(self, prompt):
+        if self.client is None:
+            print("API가 로드되지 않았습니다.")
+            return None
+        self.prompt = prompt
+        return self.prompt
 
-client = OpenAI(api_key=api_key)
+    def gptOutput(self):
+        if self.prompt is None:
+            print("gptInput 메서드로 질문하세요.")
+            exit()
 
-prompt = "안녕"
+        completion = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a English teacher. If there are any problems with my English expressions, please keep correcting me."},  # 역할 부여
+                {"role": "user", "content": self.prompt}
+            ]
+        )
+        self.answer = completion.choices[0].message.content
+        return self.answer
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": prompt}
-    ]
-)
 
-print(completion.choices[0].message.content)
+# buds = GPT('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')
+# buds.gptInput("hello")
+# result = buds.gptOutput()
+# print(result)
