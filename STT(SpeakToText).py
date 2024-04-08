2f279641cee270114f457fe0503b7cd7bbda3de4
yutak <soano2428@gmail.com>
2024-04-08 14:25:20 +0900

    24.04.08

--- STT(SpeakToText).py
+++ STT(SpeakToText).py
@@ -2,23 +2,24 @@ import speech_recognition as sr
 
 class STT:
     def __init__(self, energy, speak_time_out, analyize_ambient_duration):
-        self.rec_static_instance = sr.Recognizer()
-        self.rec_static_instance.energy_threshold = energy
+        self.energy_threshold = energy
         self.timeout = speak_time_out
         self.ambient_duration = analyize_ambient_duration
         print("음성 인식을 시작합니다")
         
     def get_audio(self):
-        rec = self.rec_static_instance
+        rec = sr.Recognizer()
+        rec.energy_threshold = self.energy_threshold
         text = ""
         with sr.Microphone() as source:
             try:
                 rec.listen(source, timeout = self.timeout)
-                #noise_cancelled_audio = rec.adjust_for_ambient_noise(source=audio_data, duration=0.5)
-                rec.adjust_for_ambient_noise(source=source, duration=self.ambient_duration)
+                print("음성획득")
+                #rec.adjust_for_ambient_noise(source=source, duration=self.ambient_duration)
                 text = rec.recognize_google(source, language="ko")
+                print("텍스트 변환성공")
             except Exception as e:
-                pass
+                text=".."
         return text
     
     def run(self):
@@ -30,5 +31,5 @@ class STT:
             else:
                 print(text)
 
-temp = STT(1000, 1.0, 0.5)
+temp = STT(500, 1.0, 0.5)
 temp.run()
--- SpeakToText.py
+++ SpeakToText.py
@@ -1,15 +1,19 @@
 import speech_recognition as sr
 #import pyaudio
 
-rec = sr.Recognizer()
-rec.energy_threshold = 2000
 print("Run")
+num = 0
+
 while True:
+    num +=1
+    rec = sr.Recognizer()
+    rec.energy_threshold = 500
+    text = ""
     with sr.Microphone() as source:
         try:
-            audio_data = rec.listen(source, timeout=0.5)
+            audio_data = rec.listen(source, timeout=0.3, phrase_time_limit=5)
             text = rec.recognize_google(audio_data, language = "ko")
         except:
-            print("..")
             text = ""
         print(text)
+    print(num)
